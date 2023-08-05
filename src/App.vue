6edd58696ec8178e96370f2dd0f95b3b93c62b56
zhangxuefeng <2367807010@qq.com>
2023-08-05 17:37:49 +0800

    feat: update header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <el-header>header</el-header>
+    <el-header>
+      <Header />
+    </el-header>
     <el-container>
-      <el-aside width="200px">菜单</el-aside>
+      <el-aside width="200px">
+        <MenuNav />
+      </el-aside>
       <el-main>
         <router-view />
       </el-main>
@@ -10,12 +14,47 @@
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import MenuNav from "@/components/MenuNav.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    MenuNav,
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  .el-header {
+    padding: 0;
+    position: sticky;
+    width: 100%;
+    top: 0;
+    z-index: 999;
+    background-color: #ffffff;
+  }
+  .el-container {
+    z-index: 100;
+    .el-aside {
+      position: fixed;
+      top: 60px;
+      height: 980px;
+      width: 150px !important;
+      background-color: rgb(84, 92, 100);
+    }
+  }
 }
 </style>
---
+++ src/components/MenuNav.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-menu
+    default-active="1"
+    router="true"
+    class="el-menu-vertical-demo"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-menu-item :index="menu.path" v-for="menu in menuList" :key="menu.id">
+      <template slot="title">
+        <i :class="menu.icon"></i>
+        <span>{{ menu.title }}</span>
+      </template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "MenuNav",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      menuList: [
+        {
+          id: "1",
+          title: "概况",
+          path: "/",
+          icon: "el-icon-location",
+        },
+        {
+          id: "2",
+          title: "会员",
+          path: "about",
+          icon: "el-icon-setting",
+        },
+        {
+          id: "3",
+          title: "营销",
+          path: "/",
+          icon: "el-icon-location",
+        },
+        {
+          id: "4",
+          title: "推广",
+          path: "about",
+          icon: "el-icon-setting",
+        },
+        {
+          id: "5",
+          title: "数据",
+          path: "/",
+          icon: "el-icon-location",
+        },
+        {
+          id: "6",
+          title: "订单",
+          path: "about",
+          icon: "el-icon-setting",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.el-menu {
+  border: none;
+}
+</style>
