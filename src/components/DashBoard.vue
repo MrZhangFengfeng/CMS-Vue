46bc56126b952712581deb2fece336d15736437a
zhangxuefeng <2367807010@qq.com>
2023-08-06 00:00:31 +0800

    feat: add echarts

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -4,16 +4,23 @@
       <el-col :span="16"><BusinessOverView /></el-col>
       <el-col :span="8"><BusinessOverView /></el-col>
     </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8"><BusinessMap /></el-col>
+      <el-col :span="6">1</el-col>
+      <el-col :span="6">1</el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import BusinessOverView from "@/components/home/BusinessOverView.vue";
+import BusinessMap from "@/components/home/BusinessMap.vue";
 
 export default {
   name: "DashBoard",
   components: {
     BusinessOverView,
+    BusinessMap,
   },
   props: {
     msg: String,
---
+++ src/components/home/BusinessMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-card>
+    <div id="map" style="width: 300px; height: 400px"></div>
+  </el-card>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: "BusinessMap",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    var myChart = echarts.init(document.getElementById("map"));
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+    this.$nextTick(function () {
+      // 仅在整个视图都被渲染之后才会运行的代码
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
