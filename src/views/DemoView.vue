aa79f58ba18aad8c2ab33c6bc6a959b3a836ba47
zhangxuefeng <2367807010@qq.com>
2023-08-12 13:45:46 +0800

    feat: update demo page

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="page-wrap">
-        <el-card class="card-wrap" style="">
-            <el-link type="primary" href="/#/demo/life-cycle"
-                ><i class="el-icon-ice-cream-round"></i> 生命周期</el-link
-            >
-        </el-card>
+        <div v-for="instance in instanceList" :key="instance.label">
+            <div class="label">{{ instance.label }}</div>
+            <el-divider></el-divider>
+            <div class="card-wrap">
+                <el-card
+                    class="card-item"
+                    v-for="subInstance in instance.subInstanceList"
+                    :key="subInstance.title"
+                >
+                    <el-link type="primary" :href="subInstance.href"
+                        ><i :class="subInstance.icon"></i>
+                        {{ subInstance.title }}</el-link
+                    >
+                </el-card>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,8 +29,74 @@ export default {
         return {
             instanceList: [
                 {
-                    imageUrl:
-                        "https://gw.alicdn.com/imgextra/i2/O1CN01TxWlTn1knvrWtaM1Z_!!6000000004729-2-tps-1000-450.png",
+                    label: "Basic",
+                    subInstanceList: [
+                        {
+                            title: "生命周期",
+                            icon: "el-icon-ice-cream-round",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "watch 和 computed",
+                            icon: "el-icon-sugar",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "组件间数据传递",
+                            icon: "el-icon-cold-drink",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "动态组件 & 异步组件",
+                            icon: "el-icon-cold-drink",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "slots() 和 children 对比",
+                            icon: "el-icon-hot-water",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "过滤器",
+                            icon: "el-icon-tableware",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "mixin",
+                            icon: "el-icon-dish",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "自定义指令",
+                            icon: "el-icon-cloudy-and-sunny",
+                            href: "/#/demo/life-cycle",
+                        },
+                        {
+                            title: "响应式原理",
+                            icon: "el-icon-sunrise",
+                            href: "/#/demo/life-cycle",
+                        },
+                    ],
+                },
+                {
+                    label: "生态系统",
+                    subInstanceList: [
+                        {
+                            title: "生命周期",
+                            icon: "el-icon-ice-cream-round",
+                            href: "/#/demo/life-cycle",
+                        },
+                    ],
+                },
+                {
+                    label: "开发实践",
+                    subInstanceList: [
+                        {
+                            title: "生命周期",
+                            icon: "el-icon-ice-cream-round",
+                            href: "/#/demo/life-cycle",
+                        },
+                    ],
                 },
             ],
         };
@@ -27,11 +104,18 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.page-wrap {
+.label {
+    font-size: 20px;
+    font-weight: 500;
+}
+.card-wrap {
     display: flex;
     flex-wrap: wrap;
-    .card-wrap {
+    .card-item {
         margin-bottom: 20px;
+        text-align: center;
+        margin-right: 20px;
+        padding: 0 20px;
     }
 }
 </style>
