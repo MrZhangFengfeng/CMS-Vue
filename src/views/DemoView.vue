935b4b97c207f2725c23d4d46ad3bfd62ec5369e
zhangxuefeng <2367807010@qq.com>
2023-08-12 12:26:37 +0800

    feat: update

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,18 +1,9 @@
 <template>
-    <div>
-        <el-card class="card-wrap">
-            <el-carousel :interval="5000" arrow="always">
-                <el-carousel-item
-                    v-for="instance in instanceList"
-                    :key="instance.name"
-                >
-                    <img :src="instance.imageUrl" />
-                </el-carousel-item>
-            </el-carousel>
-        </el-card>
-
-        <el-card class="card-wrap">
-            <el-link type="primary" href="/#/demo/life-cycle">生命周期</el-link>
+    <div class="page-wrap">
+        <el-card class="card-wrap" style="">
+            <el-link type="primary" href="/#/demo/life-cycle"
+                ><i class="el-icon-ice-cream-round"></i> 生命周期</el-link
+            >
         </el-card>
     </div>
 </template>
@@ -25,28 +16,22 @@ export default {
     components: {},
     data: function () {
         return {
-            instanceList: [],
+            instanceList: [
+                {
+                    imageUrl:
+                        "https://gw.alicdn.com/imgextra/i2/O1CN01TxWlTn1knvrWtaM1Z_!!6000000004729-2-tps-1000-450.png",
+                },
+            ],
         };
     },
 };
 </script>
 <style scoped lang="scss">
-.card-wrap {
-    margin-bottom: 20px;
-}
-.el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-}
-
-.el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
+.page-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    .card-wrap {
+        margin-bottom: 20px;
+    }
 }
 </style>
